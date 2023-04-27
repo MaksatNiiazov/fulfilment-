28905caeea8ba6ffa84434f804b62ad134c6290d
Maksat Niiazov <niyazov37@gmail.com>
2023-04-27 12:46:23 +0600

    '11%'

--- static/js/script.js
+++ static/js/script.js
@@ -55,3 +55,40 @@ function changeSlide(index) {
     sliderWrapper.style.transform = `translateX(${distance}px)`;
   }
   
+
+
+
+  // Находим якорь и скрываем его при загрузке страницы
+var backToTopButton = document.querySelector('#back-to-top');
+backToTopButton.style.display = 'none';
+
+// Функция, которая показывает или скрывает якорь в зависимости от прокрутки страницы
+function toggleBackToTopButton() {
+  if (window.pageYOffset > 300) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+}
+
+// Функция, которая прокручивает страницу вверх с плавным эффектом
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Добавляем обработчик события scroll, который вызывает функцию toggleBackToTopButton()
+window.addEventListener('scroll', toggleBackToTopButton);
+
+// Добавляем обработчик события click для якоря, который прокручивает страницу вверх с плавным эффектом
+backToTopButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  scrollToTop();
+});
+
+// Добавляем условие, чтобы якорь не был виден, когда пользователь находится вверху страницы
+if (document.documentElement.scrollTop === 0) {
+  backToTopButton.style.display = 'none';
+}
