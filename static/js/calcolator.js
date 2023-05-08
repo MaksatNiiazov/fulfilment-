528fec52ba4d593e692b4bdd01802cfd22c90a58
Maksat Niiazov <niyazov37@gmail.com>
2023-05-08 16:32:52 +0600

    mediafix

--- static/js/calcolator.js
+++ static/js/calcolator.js
@@ -1,63 +1,71 @@
-var cart = {};
-
-function addToCart(serviceId) {
-  var serviceName = document.querySelector('#' + serviceId + '_name').textContent;
-  var servicePrice = parseInt(document.querySelector('#' + serviceId + '_price').textContent);
-
-  if (cart.hasOwnProperty(serviceId)) {
-    cart[serviceId]++;
-  } else {
-    cart[serviceId] = 1;
-  }
-
-  updateCart();
-}
-
-function updateCart() {
-  var total = 0;
-  var cartList = document.querySelector('#cart_list');
-  cartList.innerHTML = '';
-
-  for (var serviceId in cart) {
-    var serviceName = document.querySelector('#' + serviceId + '_name').textContent;
-    var servicePrice = parseInt(document.querySelector('#' + serviceId + '_price').textContent);
-    var serviceQuantity = cart[serviceId];
-    var cartItem = document.createElement('li');
-    var cartItemQuantityInput = document.createElement('input');
-    cartItemQuantityInput.type = 'number';
-    cartItemQuantityInput.value = serviceQuantity;
-    cartItemQuantityInput.min = '1';
-
-    cartItemQuantityInput.addEventListener('input', function(event) {
-      var quantity = parseInt(event.target.value);
-      if (quantity > 0) {
-        cart[serviceId] = quantity;
-      } else {
-        delete cart[serviceId];
-      }
-      updateCart();
-    });
-
-    var deleteButton = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.textContent = 'Удалить';
-    deleteButton.addEventListener('click', function(event) {
-      delete cart[serviceId];
-      updateCart();
-    });
-
-    cartItem.textContent = serviceName + ': ';
-    cartItem.appendChild(cartItemQuantityInput);
-    cartItem.textContent += serviceQuantity + ' x ' + servicePrice + ' рублей = ' + serviceQuantity * servicePrice + ' рублей';
-    cartItem.appendChild(deleteButton);
-
-    cartList.appendChild(cartItem);
-
-    total += serviceQuantity * servicePrice;
-  }
-
-  var averagePrice = total / Object.keys(cart).length; // вычисляем среднее арифметическое
-  var totalWithAverage = total + ' рублей (среднее: ' + averagePrice.toFixed(2) + ' рублей)'; // добавляем среднее значение в общую сумму
-
-  document.querySelector('#total').value = totalWithAverage;
-}
+ // Находим элементы таблицы и их содержимое
+ const table = document.getElementById("services");
+ const tbody = table.querySelector("tbody");
+ const rows = tbody.querySelectorAll("tr");
+ 
+ // Находим элементы для вывода итоговых результатов
+ const totalElement = document.getElementById("total");
+ const averageElement = document.getElementById("average");
+ 
+ // Находим элементы кнопок увеличения и уменьшения количества услуг
+ const decrementButtons = document.querySelectorAll(".decrement");
+ const incrementButtons = document.querySelectorAll(".increment");
+ 
+ // Объявляем переменные для хранения общей стоимости и количества услуг
+ let total = 0;
+ let count = 0;
+ 
+ // Создаем функцию для обновления итоговых результатов
+ function updateTotals() {
+   totalElement.textContent = `Общая стоимость: ${total}`;
+   averageElement.textContent = `Средняя стоимость: ${total / count}`;
+ }
+ 
+ // Добавляем обработчики событий для кнопок увеличения и уменьшения количества услуг
+ decrementButtons.forEach(button => {
+   button.addEventListener("click", event => {
+     const input = event.target.parentNode.querySelector("input");
+     const value = parseInt(input.value);
+     if (value > 0) {
+       input.value = value - 1;
+       const row = event.target.closest("tr");
+       const price = parseInt(row.querySelector("td:nth-of-type(3)").textContent.slice(1));
+       total -= price;
+       count -= 1;
+       updateTotals();
+     }
+   });
+ });
+ 
+ incrementButtons.forEach(button => {
+   button.addEventListener("click", event => {
+     const input = event.target.parentNode.querySelector("input");
+     const value = parseInt(input.value);
+     input.value = value + 1;
+     const row = event.target.closest("tr");
+     const price = parseInt(row.querySelector("td:nth-of-type(3)").textContent.slice(1));
+     total += price;
+     count += 1;
+     updateTotals();
+   });
+ });
+ 
+ // Добавляем обработчики событий для изменения полей ввода количества услуг
+ rows.forEach(row => {
+   const input = row.querySelector("input");
+   input.addEventListener("input", event => {
+     const value = parseInt(input.value);
+     if (value >= 0) {
+       const price = parseInt(row.querySelector("td:nth-of-type(3)").textContent.slice(1));
+       const prevValue = parseInt(input.getAttribute("data-prev-value"));
+       total -= price * (prevValue || 0);
+       total += price * value;
+       count += value - (prevValue || 0);
+       input.setAttribute("data-prev-value", value);
+       updateTotals();
+     } else {
+       input.value = input.getAttribute("data-prev-value") || 0;
+     }
+   });
+ });
+ 
